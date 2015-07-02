6b5590399ed67f2a4ba1132c8e74d675e35854bc
Todd Lynam <tlynam@gmail.com>
2015-07-02 14:43:23 -0700

    Refactorings

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,7 +1,16 @@
 class InputsController < ApplicationController
   def index
     @inputs = Input.all.paginate(page: params[:page], per_page: 10).order('created_at DESC')
-    @images = Dir.glob("app/assets/images/animations/*.gif")
+   
+    @images_info = []
+    images = Dir.glob("public/images/animations/*.gif")
+    images.each do |image|
+      image_info = {}
+      date = image.split('/').last.gsub(".gif","")
+      image_info[:date] = date
+      image_info[:path] = "images/animations/#{date}.gif"
+      @images_info << image_info
+    end
   end
 
   def collect
--- app/models/input.rb
+++ app/models/input.rb
@@ -3,37 +3,46 @@ include Magick
 class Input < ActiveRecord::Base
 
   def self.collect
-    sensor_output = %x(sudo ./lib/sensor.a)
-    values = sensor_output.scan(/(\d+.\d+)/).flatten
+    sensor_output = `sudo ./lib/sensor.a`
+    values = sensor_output.scan(/(\d+.\d+)/).flatten.map { |val| Float(val) }
     
-    { temperature: Float(values[0]), humidity: Float(values[1]), dewpoint: Float(values[2]) }
+    { temperature: values[0], humidity: values[1], dewpoint: values[2] }
+  end
+
+  def fahrenheit_temperature
+    (self.temperature * 1.8 + 32).round(2)
+  end
+
+  def fahrenheit_dewpoint
+    (self.dewpoint * 1.8 + 32).round(2)
   end
 
   def self.save_picture
+    `fswebcam -r 1280x960 --flip h,v --no-banner public/images/daily/#{Time.now.to_i}.jpg`
+  end
+
+  def self.daytime?
     sun_times = SunTimes.new
     rise = sun_times.rise(Date.current, 47, -122).localtime
     set = sun_times.set(Date.current + 1, 47, -122).localtime
     daylight = rise..set
 
-    if daylight.cover? Time.zone.now
-      %x(fswebcam -r 1280x960 --flip h,v --no-banner app/assets/images/daily/#{Time.now.to_i}.jpg)
-    end
+    daylight.cover? Time.zone.now
   end
 
   def self.collect_and_save
-    input = Input.new self.collect
-    input.save
+    Input.create! collect
 
-    self.save_picture
+    save_picture if daytime?
   end
 
   def self.generate_chart_series
     temperature_series, humidity_series, dewpoint_series = {}, {}, {}
 
     Input.all.each do |input|
-      temperature_series[input.created_at] = input.temperature * 1.8 + 32
+      temperature_series[input.created_at] = input.fahrenheit_temperature
       humidity_series[input.created_at] = input.humidity
-      dewpoint_series[input.created_at] = input.dewpoint * 1.8 + 32
+      dewpoint_series[input.created_at] = input.fahrenheit_dewpoint
     end
 
     [
@@ -44,9 +53,9 @@ class Input < ActiveRecord::Base
   end
 
   def self.create_animation
-    images = Dir.glob("app/assets/images/daily/*.jpg")
+    images = Dir.glob("public/images/daily/*.jpg")
     anim = ImageList.new(*images)
-    anim.write("app/assets/images/animations/#{Date.current.to_s}.gif")
+    anim.write("public/images/animations/#{Date.current.to_s}.gif")
     File.delete(*images)
   end
 end
